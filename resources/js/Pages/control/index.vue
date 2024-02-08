ee8a20af2bd0421553eace0789bae3f71c39964b
lino <nearlino20@gmail.com>
2024-02-08 00:50:31 -0500

    dd

--- resources/js/Pages/control/index.vue
+++ resources/js/Pages/control/index.vue
@@ -1,10 +1,19 @@
 <template>
     <AdminLayout>
-        <v-container fluid> </v-container>
+        <v-container fluid>
+            .sss
+            <v-card>
+                <v-card-text>
+
+                    <qrcode-stream @detect="onDetect"></qrcode-stream>
+                </v-card-text>
+            </v-card>
+        </v-container>
     </AdminLayout>
 </template>
 <script setup>
 import AdminLayout from "@/layouts/AdminLayout.vue";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
 
 const props = defineProps({
     title: String,
@@ -17,4 +26,8 @@ const props = defineProps({
     doctors: [String, Number],
     posts: [String, Number],
 });
+
+const onDetect = (decoded) => {
+    console.log(decoded);
+};
 </script>
